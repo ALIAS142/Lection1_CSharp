663d233ed609223691df725efc0247ad1e73ae14
Anna <kochurovaav@gmail.com>
2023-01-30 20:53:39 +0200

    Sixth folder is added

---
+++ Example06/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine("Hey!!! Mary, How are you???");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
